cf8ef2d8f7444fa90e166f9a0aacd1d59382230d
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-18 20:42:59 -0300

    Imported SearchBar on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Miss me yet?</h1>
+      <SearchBar/>
     </div>
   );
 }
