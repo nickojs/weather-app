1bd91c4e981b1b3119b9db3035e3a0a9fb9b13ff
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-18 13:36:30 -0300

    Using the custom axios instance on WeatherController

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -1,4 +1,4 @@
-const axios = require('axios').default;
+const axios = require('../helpers/axios');
 
 class WeatherController { 
   constructor() { }
@@ -6,21 +6,17 @@ class WeatherController {
   async getWeatherByCity(req, res, next) {
     try {
       const { city, state, country } = req.body;
-      const weatherRequest = await axios({
-        method: 'get',
-        url: `${process.env.WEATHER_URL}${city},${state},${country}
-        &appid=${process.env.WEATHER_API_KEY}&lang=pt_br`,
-        responseType: 'json'
-      });
+      const weatherRequest = await axios
+        .get(`?q=${city},${state},${country}&appid=${process.env.API_KEY}&lang=pt_br`);
       const weatherData = weatherRequest.data;
-      
+
       const payload = {
         location: { city, state },
         weather: weatherData.weather[0], 
         main: weatherData.main, 
         wind: weatherData.wind
       };
-      
+
       res.status(200).json({ data: payload });
     } catch (error) {
       console.log(error);
