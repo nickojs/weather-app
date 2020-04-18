2d5ec8aec2636df5b9afa4fa3db3f2484fd280fa
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-18 13:13:32 -0300

    getWeatherByCity now actually returning parsed data

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -6,14 +6,28 @@ class WeatherController {
   async getWeatherByCity(req, res, next) {
     try {
       const { city, state, country } = req.body;
-      const weatherData = await axios.get(process.env.WEATHER_URL + 
-        `${city},${state},${country}` + '&appid=' + process.env.WEATHER_API_KEY)
-      res.status(200).json({ weatherData });
+      const weatherRequest = await axios({
+        method: 'get',
+        url: `${process.env.WEATHER_URL}${city},${state},${country}
+        &appid=${process.env.WEATHER_API_KEY}&lang=pt_br`,
+        responseType: 'json'
+      });
+      const weatherData = weatherRequest.data;
+      
+      const payload = {
+        location: { city, state },
+        weather: weatherData.weather[0], 
+        main: weatherData.main, 
+        wind: weatherData.wind
+      };
+      
+      res.status(200).json({ data: payload });
     } catch (error) {
       console.log(error);
       next(error);
     }
-  }
+  };
+
 }
 
 module.exports = new WeatherController();
