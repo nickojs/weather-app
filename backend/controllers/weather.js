779f94d7e53785ba08a66988e35cc64b4c913a0c
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-18 12:44:49 -0300

    Created getWeatherByCity method

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -1,6 +1,17 @@
 class WeatherController { 
   constructor() { }
 
+  async getWeatherByCity(req, res, next) {
+    try {
+      const { city, state, country } = req.body;
+      const weatherData = await fetch(process.env.WEATHER_URL + 
+        `${city},${state},${country}` + '&appid=' + process.env.WEATHER_API_KEY)
+      res.status(200).json({ weatherData });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
 }
 
 module.exports = WeatherController;
