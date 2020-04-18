cfe344124844b5254511eb354885eb612d3823e2
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-18 20:54:45 -0300

    Fetching UFs information for the SearchBar's select options

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './styles.js';
 
 const Searchbar = props => {
+  const [estados, setEstados] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const ufs = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+      const ufsData = await ufs.json();
+      const parsedUfs = ufsData.map(uf => uf.sigla).sort((a, b) => a > b);
+      setEstados(parsedUfs);
+    }
+    fetchData();
+  }, []);
 
   return(
     <S.SearchBarContainer>
