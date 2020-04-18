0f6cbeb01503209aae7eaa0318ff5ed567577446
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-18 13:12:21 -0300

    Imported weatherRoutes on app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const weatherRoutes = require('./routes/weather');
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use(bodyParser.json());
 app.get('/', (req, res, next) => {
   res.send({ message: 'root route' });
 });
+
+//weather routes
+app.use('/weather', weatherRoutes)
 
 //error handling
 app.use((err, req, res, next) => {
