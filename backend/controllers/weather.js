941dbf4685f979bcfd5c57568ba9a8620a78736e
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-18 14:16:41 -0300

    Developed method to get weather by lonlat

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -22,6 +22,28 @@ class WeatherController {
     }
   };
 
+  async getWeatherByLocation(req, res, next) {
+    try {
+      const { lat, lon } = req.body;
+      const weatherData = await getWeather(lat, lon);
+  
+      const payload = {
+        location: {
+          city: weatherData.name,
+          country: weatherData.sys.country
+        },
+        weather: weatherData.weather[0], 
+        main: weatherData.main, 
+        wind: weatherData.wind
+      };
+  
+      res.status(200).json({ data: payload });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+
 }
 
 module.exports = new WeatherController();
