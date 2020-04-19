3e1310c6b4588a88f564d2911ff1c9c60010970c
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-19 20:00:24 -0300

    Checking for validity of state (UF) on validate-city, also added some 'documentation' via comments

--- backend/helpers/validate-city.js
+++ backend/helpers/validate-city.js
@@ -2,31 +2,35 @@ const axios = require('../helpers/axios-ibge');
 
 const validateCity = async (_, { req }) => {
   try {
+    //"parses" the data to uppercase to avoid false positives
     const { city, state } = req.body;
     const parsedCity = city.toUpperCase();
     const parsedState = state.toUpperCase();
     
+    //fetches a list of all brazilian states
     const stateList = await axios.get('localidades/estados');
     const stateListData = await stateList.data;
-
-    const currentState   = stateListData.filter(uf => uf.sigla === parsedState);
+    
+    //search for current state on fetched list
+    const currentState = stateListData.filter(uf => uf.sigla === parsedState);
+    if (currentState.length === 0)
+      return Promise.reject(`Estado ${state} é invalido`);
     const currentStateId = currentState[0].id;
 
+    //fetches a list of cities based on selected state
     const citiesByState = await axios.get(
       `localidades/estados/${currentStateId}/municipios`
     );
     const citiesByStateData = citiesByState.data.map(
-      city => city.nome.toUpperCase()
-    );
-
+      city => city.nome.toUpperCase());
+    
+    //verifies if current city exists on this state, using previous list
     const locateCity = citiesByStateData.find(c => c === parsedCity);
     if(!locateCity) 
       return Promise.reject(`Cidade '${city}' não encontrada no estado ${state}`);
-
   } catch (error) {
     throw new Error(error);
   }
-
-}
+};
 
 module.exports = validateCity;
