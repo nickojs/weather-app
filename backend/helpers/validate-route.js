aa3f6d3f8bca011f84f1198ac41a09de2e3ef371
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-19 18:29:11 -0300

    Created middleware to validate routes

---
+++ backend/helpers/validate-route.js
@@ -0,0 +1,18 @@
+const { validationResult } = require('express-validator');
+const ErrorHandler = require('../models/http-error');
+
+module.exports = {
+  default: (req, res, next) => {
+    const errors = validationResult(req);
+    const errorsMsg = errors.array().map(err => err.msg).join(', ');
+    if(!errors.isEmpty())
+      return next(new ErrorHandler(errorsMsg, 422));
+    next();
+  },
+  image: (req, res, next) => {
+    if(!req.file) {
+      return next(new ErrorHandler('No image provided', 422));
+    }
+    next();
+  }
+}
