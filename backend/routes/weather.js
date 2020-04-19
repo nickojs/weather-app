01bfc168aa21b90c7f4145ca563955a722f80f19
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-19 19:32:31 -0300

    Created custom validator on the weather route to check if city exists within its state (UF)

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const WeatherController = require('../controllers/weather');
 const { body } = require('express-validator');
 const validate = require('../helpers/validate-route');
+const axios = require('axios');
 
 router.post('/', 
 [
@@ -11,6 +12,31 @@ router.post('/',
   body('state').isLength({ min: 2, max: 2 })
     .withMessage('UF possui dois dígitos')
 ], validate.default,
+  body('city').custom(async (_, { req }) => {
+    try {
+      const { city, state } = req.body;
+      const thisCity = city.toUpperCase();
+      const stateList = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+      const stateListData = await stateList.data;
+  
+      const currentState = stateListData.filter(uf => uf.sigla === state);
+      const currentStateId = currentState[0].id;
+  
+      const citiesByState = await axios.get(`
+        https://servicodados.ibge.gov.br/api/v1/localidades/estados/${currentStateId}/municipios
+      `);
+      const citiesByStateData = citiesByState.data.map(city => city.nome.toUpperCase());
+  
+      const findCity = citiesByStateData.find(c => c === thisCity);
+      
+      if(!findCity) {
+        return Promise.reject(`Cidade '${city}' não encontrada no estado ${state}`)
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }),
+   validate.default,
    WeatherController.getWeatherByCity
 );
 
