ce70c6814de7c1d22fdbaa6cf55f99bf5e1e8e1f
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-19 18:29:56 -0300

    Added express-validator on weather route

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const WeatherController = require('../controllers/weather');
+const { body } = require('express-validator');
+const validate = require('../helpers/validate-route');
 
-router.post('/', WeatherController.getWeatherByCity);
+router.post('/', 
+[
+  body('city').not().isEmpty()
+  .withMessage('Insira uma cidade'),
+  body('state').isLength({ min: 2, max: 2 })
+    .withMessage('UF possui dois dígitos')
+], validate.default,
+   WeatherController.getWeatherByCity
+);
 
 router.post('/by-location', WeatherController.getWeatherByLocation);
 
