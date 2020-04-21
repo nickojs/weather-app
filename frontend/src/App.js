712beafb3ec3b2edf975947bb8997db2b84bef82
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-21 18:01:24 -0300

    Imported weatherCard on app.js to mock it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
+import WeatherCard from './components/WeatherCard/WeatherCard';
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
+      <WeatherCard />
     </div>
   );
 }
