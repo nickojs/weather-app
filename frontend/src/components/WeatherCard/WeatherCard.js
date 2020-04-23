c4f5730ca6e41199f1d8b75c507bdda13cbff0f9
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-23 00:07:02 -0300

    Checking for data before loading WeatherCard

--- frontend/src/components/WeatherCard/WeatherCard.js
+++ frontend/src/components/WeatherCard/WeatherCard.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import mockData from '../../mock.json';
 import Location from './Location';
 import Description from './Description';
 import WeatherInfo from './WeatherInfo';
 import * as S from './styles';
 
-const WeatherCard = (props) => {
-  const { main, location, weather } = mockData.data;
-  console.log(main, location, weather);
-
-  return (
+const WeatherCard = ({ weatherData }) => (
+  weatherData ? (
     <S.WeatherContainer>
-      <Location data={location} />
-      <Description data={weather} />
-      <WeatherInfo data={main} />
+      <Location data={weatherData.location} />
+      <Description data={weatherData.weather} />
+      <WeatherInfo data={weatherData.main} />
     </S.WeatherContainer>
-  );
-};
+  ) : null
+);
 
 export default WeatherCard;
