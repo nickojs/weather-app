04982293a5f565c1df40c0a4bac8569401043642
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-22 21:33:29 -0300

    Start to style WeatherCard component

---
+++ frontend/src/components/WeatherCard/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const WeatherContainer = styled.div`
+  margin: 12px auto;
+  padding: 12px;
+  width: 30%;
+  max-width: 550px;
+  box-sizing: border-box;
+  text-align: center;
+
+  border: 1px solid black;
+  border-radius: 12px;
+`;
