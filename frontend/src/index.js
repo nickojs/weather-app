d7986583e176f206780ed68fbd7dd9ff1b26373d
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-02 17:05:10 -0300

    Added redux thunk to the redux flow

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
 import searchReducer from './store/reducers/search';
 
-const store = createStore(
-  searchReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const store = createStore(searchReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <React.StrictMode>
