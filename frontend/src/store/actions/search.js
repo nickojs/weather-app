96713e91e7285d3c4fe0421991c04583f2a7848b
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-02 18:31:06 -0300

    New action creators (and types) for fetching weather data

--- frontend/src/store/actions/search.js
+++ frontend/src/store/actions/search.js
@@ -1,5 +1,5 @@
 import * as actions from './actionTypes';
-import { fetchUf } from '../../helpers/fetch-data';
+import { fetchUf, fetchWeather } from '../../helpers/fetch-data';
 
 export const loadUfListSuccess = (ufList) => ({
   type: actions.LOAD_UFLIST_SUCCESS,
@@ -24,3 +24,22 @@ export const updateLocation = (location) => ({
   type: actions.UPDATE_LOCATION,
   location
 });
+
+export const fetchWeatherFail = (error) => ({
+  type: actions.LOAD_UFLIST_FAIL,
+  error
+});
+
+export const fetchWeatherSuccess = (data) => ({
+  type: actions.LOAD_UFLIST_FAIL,
+  weather: data
+});
+
+export const fetchWeatherHandler = (location) => async (dispatch) => {
+  try {
+    const data = await fetchWeather(location);
+    dispatch(fetchWeatherFail(data));
+  } catch (error) {
+    dispatch(fetchWeatherFail(error));
+  }
+};
