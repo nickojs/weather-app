c407e4187283a4d1a700185ce87b5213a043a0d6
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-03 15:57:53 -0300

    Added more styling to ErrorMessage component

--- frontend/src/components/UI/styles.js
+++ frontend/src/components/UI/styles.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
 export const ErrorMessage = styled.p`
-  color: red;
+  display: block;
+  margin: 0 auto;
+  padding: 8px;
+  margin: 4px;
+  width: 50%;
+
+  text-align: center;
+
+  color: white;
+  background: lightcoral;
+  border-radius: 8px;
 `;
