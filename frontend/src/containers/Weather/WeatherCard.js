3b206f4d1228b49acf78b2901e5309efdfc6ce8d
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-03 15:57:26 -0300

    Displaying errors on WeatherCard

--- frontend/src/containers/Weather/WeatherCard.js
+++ frontend/src/containers/Weather/WeatherCard.js
@@ -4,10 +4,11 @@ import Location from '../../components/WeatherCard/Location';
 import Description from '../../components/WeatherCard/Description';
 import WeatherInfo from '../../components/WeatherCard/WeatherInfo';
 import Loader from '../../components/UI/Loader';
+import ErrorMessage from '../../components/UI/ErrorMessage';
 import * as S from './styles';
 
 const WeatherCard = () => {
-  const { weather, loading } = useSelector((state) => state.weather);
+  const { weather, loading, error } = useSelector((state) => state.weather);
 
   let weatherC = null;
   if (weather) {
@@ -23,6 +24,7 @@ const WeatherCard = () => {
     <>
       {weatherC}
       <Loader status={loading} />
+      <ErrorMessage error={error} />
     </>
   );
 };
